631d843f705fb72637c47de2f3278c5bb84aaaa3
Malki Davis <md01@me.com>
2016-08-30 23:26:47 +0300

    working through udacicata tests

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,4 +4,23 @@ require 'csv'
 
 class Udacidata
   # Your code goes here!
+  create_finder_methods :brand, :name
+
+  @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
+  def self.create(opts={})
+    item = self.new(opts)
+    CSV.open(@@data_path, "a+") do |csv|
+      csv << [item.id, item.brand, item.name, item.price]
+    end
+    return item
+  end 
+
+  def self.all
+  	all_csv_data = []
+  	CSV.foreach(@@data_path, headers: true) do |row|
+      all_csv_data << row
+    end
+    return all_csv_data
+  end
 end
